725214f6f824c697b3743e70d3862718513b874d
sgrmhdk <31389818+SagarMahadik@users.noreply.github.com>
2023-02-09 13:27:37 +0530

    First Commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,13 @@
+import { CenterAlignedColumnContainer, Text } from "@benny-eleventy/benny-ui";
+
+export default function Index() {
+	return (
+		<CenterAlignedColumnContainer
+			width="100vw"
+			height="100vh"
+			background="lightpink"
+		>
+			<Text>Hello World</Text>
+		</CenterAlignedColumnContainer>
+	);
+}
